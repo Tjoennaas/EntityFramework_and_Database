60abdc153f51e6c7aa995eb04d02dd78f235e857
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2025-10-25 15:00:40 +0200

    Error handling

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -2,6 +2,9 @@
 
 //https://learn.microsoft.com/en-us/aspnet/core/tutorials/min-web-api?view=aspnetcore-9.0&tabs=visual-studio#the-model-and-database-context-classes
 
+// https://learn.microsoft.com/en-us/ef/core/modeling/entity-properties?tabs=data-annotations%2Cwith-nrt
+
+
 using System.ComponentModel.DataAnnotations;
 
 
